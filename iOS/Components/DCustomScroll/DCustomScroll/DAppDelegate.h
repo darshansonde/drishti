ef2d57714ac411a5527ce636b3d5e200fbfc3dce
Darshan Sonde <darshan@sourcebits.com>
2012-06-24 22:34:35 +0530

    Adding Parallax scrollview code

---
+++ iOS/Components/DCustomScroll/DCustomScroll/DAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DAppDelegate.h
+//  DCustomScroll
+//
+//  Created by Darshan Sonde on 24/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DViewController;
+
+@interface DAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) DViewController *viewController;
+
+@end
---
+++ iOS/Components/DCustomScroll/DCustomScroll/DViewController.h
@@ -0,0 +1,13 @@
+//
+//  DViewController.h
+//  DCustomScroll
+//
+//  Created by Darshan Sonde on 24/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DViewController : UIViewController
+
+@end
