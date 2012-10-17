2e4d3f20f4e1d6f699c57775f17455658910cec3
Darshan Sonde <darshan@sourcebits.com>
2012-10-17 18:22:29 +0530

    debug log

--- iOS/Components/DDefines.h
+++ iOS/Components/DDefines.h
@@ -3,4 +3,15 @@
 
 //should become a code snippet
 #define DCallToPhoneNumber(x) [[UIApplication sharedApplication] openURL:[NSURL URLWithString:x]]
+#define DCallToPhoneNumberCheck(x) [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:x]]
+
+
+#ifdef DEBUG
+#	define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#	define DLog(...)
+#endif
+
+// ALog always displays output regardless of the DEBUG setting
+#define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
 
